6c4586d94a7477e6189cada7814b7b882d45b353
Mercedes Valoni <valonimercedes@gmail.com>
2023-08-21 17:52:48 -0300

    Consigna TPI

---
+++ app.py
@@ -0,0 +1,45 @@
+# Trabajo Práctico I - Programación II
+
+
+import os
+
+print("Bienvenido!")
+respuesta = ''
+
+def menu():
+    print("1 - Gestionar Prestamo")
+    print("2 - Gestionar Devolucion")
+    print("3 - Registrar nuevo libro")
+    print("4 - Elimiar ejemplar")
+    print("5 - Mostrar ejemplares perstados")
+    print("6 - Salir")
+
+while respuesta != "salir":
+    menu()
+    opt = input("\n Ingrese la opción de menú: ")
+    os.system ("cls") #Limpiar pantalla
+    if opt.isnumeric():
+        if int(opt) == 1:
+            #completar
+            print()
+        elif int(opt) == 2:
+            #completar
+            print()
+        elif int(opt) == 3:
+            #completar
+            print()
+        elif int(opt) == 4:
+            #completar
+            print()
+        elif int(opt) == 5:
+            #completar
+            print()
+        elif int(opt) == 6:
+            respuesta = "salir"
+        else: print("Ingrese una opción válida")
+    else: 
+        print("Ingrese una opción numérica")
+    
+    input("Presione cualquier tecla para continuar....") # Pausa
+
+print("Hasta luego!.")
---
+++ bibloteca.py
@@ -0,0 +1,34 @@
+import libro as l
+
+# Crear una lista vacía para almacenar los libros
+libros = []
+
+# Añadir los diccionarios a la lista
+libros.append(l.libro1)
+libros.append(l.libro2)
+libros.append(l.libro3)
+
+def ejemplares_prestados():
+    # completar
+    return None
+
+def registrar_nuevo_libro():
+    nuevo_libro = l.nuevo_libro()
+    #completar
+    return None
+
+def eliminar_ejemplar_libro():
+    #completar
+    return None
+
+def prestar_ejemplar_libro():
+    #completar
+    return None
+
+def devolver_ejemplar_libro():
+    #completar
+    return None
+
+def nuevo_libro():
+    #completar
+    return None
