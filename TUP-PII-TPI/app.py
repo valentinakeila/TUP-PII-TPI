48b9e3339135a0a0b2cae579dd7639e8576740ab
valentinakeila <105466081+valentinakeila@users.noreply.github.com>
2023-09-03 19:17:19 -0300

    Add files via upload

---
+++ TUP-PII-TPI/app.py
@@ -0,0 +1,47 @@
+# Trabajo Práctico I - Programación II
+
+import bibloteca
+import os
+
+from bibloteca import *
+
+print("Bienvenido!")
+respuesta = ''
+
+def menu():
+    print("1 - Gestionar Prestamo")
+    print("2 - Gestionar Devolucion")
+    print("3 - Registrar nuevo libro")
+    print("4 - Elimiar ejemplar")
+    print("5 - Mostrar ejemplares perstados")
+    print("6 - Salir")
+
+while respuesta != "salir":
+    menu()
+    opt = input("\n Ingrese la opción de menú: ")
+    # os.system ("cls") #Limpiar pantalla
+    if opt.isnumeric():
+        if int(opt) == 1:
+            
+            print(ejemplares_prestados())
+        elif int(opt) == 2:
+            #completar
+            print()
+        elif int(opt) == 3:
+            #completar
+            print()
+        elif int(opt) == 4:
+            #completar
+            print()
+        elif int(opt) == 5:
+            #completar
+            print()
+        elif int(opt) == 6:
+            respuesta = "salir"
+        else: print("Ingrese una opción válida")
+    else: 
+        print("Ingrese una opción numérica")
+    
+    input("Presione cualquier tecla para continuar....") # Pausa
+
+print("Hasta luego!.")
---
+++ TUP-PII-TPI/readme.md
@@ -0,0 +1,5 @@
+# Trabajo Práctico I
+
+## Descripción
+
+Repositorio del TPI de Programación II. Hacer fork del repostorio y completar siguiendo las consigas del TPI publicadas en el CVG.
