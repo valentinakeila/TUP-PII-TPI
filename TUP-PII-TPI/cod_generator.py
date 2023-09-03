d8ed4acf60b51012ba3077d51c99521ba4567788
valentinakeila <105466081+valentinakeila@users.noreply.github.com>
2023-09-03 19:18:10 -0300

    Delete TUP-PII-TPI directory

--- TUP-PII-TPI/cod_generator.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-# get random password pf length 8 with letters, digits, and symbols
-def generar():
-    characters = string.ascii_letters + string.digits
-    cod = ''.join(random.choice(characters) for i in range(8))
-    return cod
