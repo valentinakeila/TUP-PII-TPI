c8bce0f141899de7802d6ca6ea6959db5391bd94
valentinakeila <105466081+valentinakeila@users.noreply.github.com>
2023-09-03 19:14:13 -0300

    Add files via upload

---
+++ Programacion II TPI Cataldi_Corvalan_Garrido/TUP-PII-TPI/libro.py
@@ -0,0 +1,12 @@
+# Crear un diccionario para cada libro
+libro1 = {'cod': 'CRBJsAkS', 'cant_ej_stock': 3, 'cant_ej_pr': 1, "titulo": "Cien años de soledad", "autor": "Gabriel García Márquez"}
+libro2 = {'cod': 'QgfV4j3c', 'cant_ej_stock': 4, 'cant_ej_pr': 2, "titulo": "El principito", "autor": "Antoine de Saint-Exupéry"}
+libro3 = {'cod': 'adOd09UE', 'cant_ej_stock': 1, 'cant_ej_pr': 0, "titulo": "El código Da Vinci", "autor": "Dan Brown"}
+
+def nuevo_libro():
+    #completar
+    return None
+
+def generar_codigo():
+    #completar
+    return None
